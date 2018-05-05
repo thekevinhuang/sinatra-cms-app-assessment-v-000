e7940c0495a8699073a395f9bf5fd6855576a4f4
Kevin Huang <yellow.khaos@gmail.com>
2018-05-05 23:59:19 +0000

    added player routes and erb files

--- ow-fantasy-league-v002/app/controllers/players_controller.rb
+++ ow-fantasy-league-v002/app/controllers/players_controller.rb
@@ -1,9 +1,13 @@
 class PlayersController < ApplicationController
   get '/players' do
     #shows a list of all players with link to team
+    @players = Player.all
+    erb :'/players/index'
   end
 
   get '/players/:slug' do
     #shows a specific player with heroes played
+    @player = Player.find_by_slug(params[:slug])
+    erb :'/players/show'
   end
 end
