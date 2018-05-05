ae2b3c2fecdc831759a8334b144fb4a928629e94
Kevin Huang <yellow.khaos@gmail.com>
2018-05-05 23:39:09 +0000

    Added routes/erb files for teams and players

--- ow-fantasy-league-v002/app/controllers/teams_controller.rb
+++ ow-fantasy-league-v002/app/controllers/teams_controller.rb
@@ -1,9 +1,11 @@
 class TeamsController < ApplicationController
   get '/teams' do #view list of all teams
-
+    @teams = Team.all
+    erb :'/teams/index'
   end
 
   get '/teams/:slug' do #view specific team with list of players
-
+    @team = Team.find_by_slug(params[:slug])
+    erb :'/teams/show'
   end
 end
--- ow-fantasy-league-v002/app/models/team.rb
+++ ow-fantasy-league-v002/app/models/team.rb
@@ -1,3 +1,16 @@
 class Team <ActiveRecord::Base
+  has_many :players
 
+  def slug
+    slug_name = self.name.gsub(' ', '-')
+    slug_name = slug_name.gsub(/[^-a-zA-Z0-9$_.+!*()]/, "")
+    slug_name = slug_name.downcase
+    slug_name
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find do |team|
+      team.slug == slug
+    end
+  end
 end
