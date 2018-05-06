47efb30cc9dd4f0745b3590d833f9fd0186ca2d4
Kevin Huang <yellow.khaos@gmail.com>
2018-05-06 02:09:25 +0000

    added initial logic and pages for league creation

--- ow-fantasy-league-v002/app/controllers/leagues_controller.rb
+++ ow-fantasy-league-v002/app/controllers/leagues_controller.rb
@@ -5,12 +5,23 @@ class LeaguesController < ApplicationController
     erb :'/leagues/index'
   end
 
-  get 'leagues/new' do
+  get '/leagues/new' do
     #allows a user to create a new league
+    erb :'/leagues/create'
+  end
+
+  post '/leagues/new' do
+    new_league = League.new(name: params[:league_name])
+    new_league.users << current_user
+    new_league.save
+    redirect "/leagues/#{new_league.id}"
   end
 
   get '/leagues/:id' do
     #shows a specific league with all the rosters
+    @league = League.find_by(id: params[:id])
+
+    erb :'/leagues/show'
   end
 
   get '/leagues/:id/join' do
--- ow-fantasy-league-v002/app/models/league.rb
+++ ow-fantasy-league-v002/app/models/league.rb
@@ -2,6 +2,4 @@ class League <ActiveRecord::Base
   has_many :rosters
   has_many :user_leagues
   has_many :users, through: :user_leagues
-
-
 end
