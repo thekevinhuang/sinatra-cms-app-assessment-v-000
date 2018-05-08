4734fc46e5219984c258e6a125f931272c14afff
Kevin Huang <yellow.khaos@gmail.com>
2018-05-08 02:14:23 +0000

    added simple roster creation

--- ow-fantasy-league-v002/app/controllers/application_controller.rb
+++ ow-fantasy-league-v002/app/controllers/application_controller.rb
@@ -1,22 +1,24 @@
 require './config/environment'
+require 'sinatra/flash'
 
 class ApplicationController < Sinatra::Base
+  enable :sessions
+  register Sinatra::Flash
+  configure do
+      set :public_folder, 'public'
+      set :views, 'app/views'
+      enable :sessions
+      set :session_secret, "ameizing"
+  end
 
-    configure do
-        set :public_folder, 'public'
-        set :views, 'app/views'
-        enable :sessions
-        set :session_secret, "ameizing"
+  helpers do
+    def current_user
+      User.find_by(id: session[:user_id])
     end
 
-    helpers do
-      def current_user
-        User.find_by(id: session[:user_id])
-      end
-
-      def logged_in?
-        !!current_user
-      end
+    def logged_in?
+      !!current_user
     end
+  end
 
 end
--- ow-fantasy-league-v002/app/controllers/rosters_controller.rb
+++ ow-fantasy-league-v002/app/controllers/rosters_controller.rb
@@ -1,19 +1,43 @@
 class RostersController < ApplicationController
 
-  get '/roster/new' do
+  get '/leagues/:id/rosters/new' do
+    @league = League.find_by(id: params[:id])
+
+    if current_user.leagues.include?(@league)
+      #check if current user is in this league
+      @roster = @league.rosters & current_user.rosters
+      if !@roster.empty? #checks the intersection of the rosters in the league and the user's rosters
+        redirect "/rosters/#{@roster.first.id}"
+      else
+        erb :'/rosters/create'
+      end
+    else
+      flash[:message] = "You are not a part of this league."
+      redirect "/leagues/#{@league.id}"
+    end
     #allows user to create a new roster in a league they have joined
-    #@empty_leagues = []
-    #current_user.leagues do |league|
-    #
-    #end
-    erb :'/rosters/create'
-  end
-
-  get '/roster/:id' do #shows a specific roster with players
+    #roster belongs to league
+    #roster has many players
+    #league has many rosters
+    #league has many users
+    #user has many rosters
+    #checks if current_user is a member of the current league & does not have a roster belonging to the league
 
   end
 
-  get '/roster/:id/edit' do #allows user to edit roster, if roster belongs to them
+  post '/leagues/:id/rosters/new' do
+    @league = League.find_by(id: params[:id])
+    rost = Roster.new(name: params[:roster_name])
+    rost.league = @league
+    rost.save
+    redirect "/rosters/#{rost.id}"
+  end
+
+  get '/rosters/:id' do #shows a specific roster with players
+    erb :'/rosters/show'
+  end
+
+  get '/rosters/:id/edit' do #allows user to edit roster, if roster belongs to them
 
   end
 end
