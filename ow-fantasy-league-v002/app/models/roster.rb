a7513e57937c8225d53b15ff162f1a372680cc6d
Kevin Huang <yellow.khaos@gmail.com>
2018-06-19 15:45:45 +0000

    automatically backed up by learn

--- ow-fantasy-league-v002/app/models/roster.rb
+++ ow-fantasy-league-v002/app/models/roster.rb
@@ -3,6 +3,7 @@ class Roster <ActiveRecord::Base
   has_many :roster_players
   has_many :players, through: :roster_players
   belongs_to :user
+  
 
   def contain_player?(player)
     !!self.players.find(player) #returns truthy value if the player was found in the roster
